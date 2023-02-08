46e0b55f6c5e707ac519f533c0f367e654ecfada
Misaka-blog <misakablog@protonmail.com>
2023-02-08 16:47:03 +0800

    Init.

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# xray-for-deepnote
+# xray for Deepnote
+
+## 项目特点
+
+* 本项目用于在 [Deepnote](https://deepnote.com/) 免费服务上部署 vmess / vless / trojan / shadowsocks 节点
+* 部署完成如发现不能上网，请检查域名是否被墙，可使用 Cloudflare CDN 或者 worker 解决。
+
+## 部署
+
+> 现项目使用的是多协议，原单协议项目说明已经转移至 Release 页面，请在 Release 页面进行下载
+
+* 注册 [Deepnote](https://deepnote.com/)
+* 创建一个Project，点击Environment中的Initialization，打开里面的`init.ipynb`
+* 删除里面自带的代码块并创建一个，输入下面的内容
+
+```
+!sleep infinity
+```
+
+![](https://gcore.jsdelivr.net/gh/Misaka-blog/imgs@main/20230208164251.png)
+
+* 打开 Allow Incoming Connections的开关
+* 在命令行复制粘贴，运行以下命令
+
+```shell
+wget -N https://raw.githubusercontent.com/Misaka-blog/xray-for-deepnote/main/deep.sh && bash deep.sh
+```
+
+* 节点配置如下
+
+```
+协议：Vmess / Vless / Trojan / Shadowsocks
+地址：xxxxx.deepnoteproject.com
+端口：443
+UUID/密码：8d4a8f5e-c2f7-4c1b-b8c0-f8f5a9b6c384 或自己定义的UUID
+额外ID：0
+Shadowsocks加密方式：chacha20-ietf-poly1305
+传输协议：ws
+伪装域名：xxxxx.deepnoteproject.com
+路径：/vmess（/vless、/trojan、/shadowsocks）
+传输安全：TLS
+跳过证书验证：true或false都可以
+```
+
+## 免责声明
+
+* 本程序仅供学习了解, 非盈利目的，请于下载后 24 小时内删除, 不得用作任何商业用途, 文字、数据及图片均有所属版权, 如转载须注明来源。
+* 使用本程序必循遵守部署免责声明。使用本程序必循遵守部署服务器所在地、所在国家和用户所在国家的法律法规, 程序作者不对使用者任何不当行为负责。
+
+## 赞助
+
+爱发电：https://afdian.net/a/Misaka-blog
+
+![afdian-MisakaNo の 小破站](https://user-images.githubusercontent.com/122191366/211533469-351009fb-9ae8-4601-992a-abbf54665b68.jpg)
---
+++ deep.sh
@@ -0,0 +1,250 @@
+#!/bin/bash
+
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+PLAIN='\033[0m'
+
+red() {
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+
+green() {
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+
+yellow() {
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+
+clear
+echo "#############################################################"
+echo -e "#               ${RED} Deepnote xray 一键安装脚本${PLAIN}                 #"
+echo -e "# ${GREEN}作者${PLAIN}: MisakaNo の 小破站                                  #"
+echo -e "# ${GREEN}博客${PLAIN}: https://blog.misaka.rest                            #"
+echo -e "# ${GREEN}GitHub 项目${PLAIN}: https://github.com/Misaka-blog               #"
+echo -e "# ${GREEN}Telegram 频道${PLAIN}: https://t.me/misakablogchannel             #"
+echo -e "# ${GREEN}Telegram 群组${PLAIN}: https://t.me/misakanoxpz                   #"
+echo -e "# ${GREEN}YouTube 频道${PLAIN}: https://www.youtube.com/@misaka-blog        #"
+echo "#############################################################"
+echo ""
+
+yellow "使用前请注意："
+red "1. 我已知悉本项目有可能触发 Deepnote 封号机制"
+red "2. 我不保证脚本其搭建节点的稳定性"
+read -rp "是否安装脚本？ [Y/N]：" yesno
+
+if [[ $yesno =~ "Y"|"y" ]]; then
+    rm -f web config.json
+    yellow "开始安装..."
+    wget -O temp.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+    unzip temp.zip xray
+    RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
+    mv xray web
+    wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
+    wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+    read -rp "请设置UUID（如无设置则使用脚本默认的）：" uuid
+    if [[ -z $uuid ]]; then
+        uuid="8d4a8f5e-c2f7-4c1b-b8c0-f8f5a9b6c384"
+    fi
+    cat <<EOF > config.json
+{
+    "log":{
+        "access":"/dev/null",
+        "error":"/dev/null",
+        "loglevel":"none"
+    },
+    "inbounds":[
+        {
+            "port":8080,
+            "protocol":"vless",
+            "settings":{
+                "clients":[
+                    {
+                        "id":"$uuid",
+                        "flow":"xtls-rprx-direct"
+                    }
+                ],
+                "decryption":"none",
+                "fallbacks":[
+                    {
+                        "dest":3001
+                    },
+                    {
+                        "path":"/vless",
+                        "dest":3002
+                    },
+                    {
+                        "path":"/vmess",
+                        "dest":3003
+                    },
+                    {
+                        "path":"/trojan",
+                        "dest":3004
+                    },
+                    {
+                        "path":"/shadowsocks",
+                        "dest":3005
+                    }
+                ]
+            },
+            "streamSettings":{
+                "network":"tcp"
+            }
+        },
+        {
+            "port":3001,
+            "listen":"127.0.0.1",
+            "protocol":"vless",
+            "settings":{
+                "clients":[
+                    {
+                        "id":"$uuid"
+                    }
+                ],
+                "decryption":"none"
+            },
+            "streamSettings":{
+                "network":"ws",
+                "security":"none"
+            }
+        },
+        {
+            "port":3002,
+            "listen":"127.0.0.1",
+            "protocol":"vless",
+            "settings":{
+                "clients":[
+                    {
+                        "id":"$uuid",
+                        "level":0,
+                        "email":"argo@xray"
+                    }
+                ],
+                "decryption":"none"
+            },
+            "streamSettings":{
+                "network":"ws",
+                "security":"none",
+                "wsSettings":{
+                    "path":"/vless"
+                }
+            },
+            "sniffing":{
+                "enabled":true,
+                "destOverride":[
+                    "http",
+                    "tls",
+                    "quic"
+                ],
+                "metadataOnly":false
+            }
+        },
+        {
+            "port":3003,
+            "listen":"127.0.0.1",
+            "protocol":"vmess",
+            "settings":{
+                "clients":[
+                    {
+                        "id":"$uuid",
+                        "alterId":0
+                    }
+                ]
+            },
+            "streamSettings":{
+                "network":"ws",
+                "wsSettings":{
+                    "path":"/vmess"
+                }
+            },
+            "sniffing":{
+                "enabled":true,
+                "destOverride":[
+                    "http",
+                    "tls",
+                    "quic"
+                ],
+                "metadataOnly":false
+            }
+        },
+        {
+            "port":3004,
+            "listen":"127.0.0.1",
+            "protocol":"trojan",
+            "settings":{
+                "clients":[
+                    {
+                        "password":"$uuid"
+                    }
+                ]
+            },
+            "streamSettings":{
+                "network":"ws",
+                "security":"none",
+                "wsSettings":{
+                    "path":"/trojan"
+                }
+            },
+            "sniffing":{
+                "enabled":true,
+                "destOverride":[
+                    "http",
+                    "tls",
+                    "quic"
+                ],
+                "metadataOnly":false
+            }
+        },
+        {
+            "port":3005,
+            "listen":"127.0.0.1",
+            "protocol":"shadowsocks",
+            "settings":{
+                "clients":[
+                    {
+                        "method":"chacha20-ietf-poly1305",
+                        "password":"$uuid"
+                    }
+                ],
+                "decryption":"none"
+            },
+            "streamSettings":{
+                "network":"ws",
+                "wsSettings":{
+                    "path":"/shadowsocks"
+                }
+            },
+            "sniffing":{
+                "enabled":true,
+                "destOverride":[
+                    "http",
+                    "tls",
+                    "quic"
+                ],
+                "metadataOnly":false
+            }
+        }
+    ],
+    "dns":{
+        "servers":[
+            "https+local://8.8.8.8/dns-query"
+        ]
+    },
+    "outbounds":[
+        {
+            "protocol":"freedom"
+        }
+    ]
+}
+EOF
+    nohup ./web -config=config.json &>/dev/null &
+    green "Deepnote xray 已安装完成！"
+    yellow "请认真阅读项目博客说明文档，配置出站链接！"
+    yellow "别忘记给项目点一个免费的Star！"
+    echo ""
+    yellow "更多项目，请关注：小御坂的破站"
+else
+    red "已取消安装，脚本退出！"
+    exit 1
+fi
